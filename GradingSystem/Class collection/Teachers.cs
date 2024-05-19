157f82b021a131fad5973b3dc27837b63cc92909
JenniferChauLam <167849302+JenniferChauLam@users.noreply.github.com>
2024-05-19 11:07:54 +0700

    Update Teachers.cs

--- GradingSystem/Class collection/Teachers.cs
+++ GradingSystem/Class collection/Teachers.cs
@@ -31,34 +31,70 @@ namespace GradingSystem.Class_collection
         public int Create(string firstName, string lastName, string username, string password, string email, DateTime dateOfBirth, string phoneNumber)
         {
             int result = 0;
-            using (SqlConnection connection = new SqlConnection(connectionString))
+
+            if (SearchUserName(username))
             {
-                connection.Open();
-                string query = "INSERT INTO Teachers (ID, FirstName, LastName, Username, Password, Email, DateOfBirth, PhoneNumber) " +
-                               "VALUES (@ID, @FirstName, @LastName, @Username, @Password, @Email, @DateOfBirth, @PhoneNumber)";
-                using (SqlCommand command = new SqlCommand(query, connection))
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    this.id = GenerateTeacherID();
-                    command.Parameters.AddWithValue("@ID", this.id);
-                    this.firstName = firstName;
-                    command.Parameters.AddWithValue("@FirstName", firstName);
-                    this.lastName = lastName;
-                    command.Parameters.AddWithValue("@LastName", lastName);
-                    this.username = username;
-                    command.Parameters.AddWithValue("@Username", username);
-                    this.password = password;
-                    command.Parameters.AddWithValue("@Password", password);
-                    this.email = email;
-                    command.Parameters.AddWithValue("@Email", email);
-                    this.dateOfBirth = dateOfBirth;
-                    command.Parameters.AddWithValue("@DateOfBirth", dateOfBirth);
-                    this.phoneNumber = phoneNumber;
-                    command.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
-                    command.ExecuteNonQuery();
-                    result = command.ExecuteNonQuery();
+                    connection.Open();
+                    string query = "INSERT INTO Teachers (ID, FirstName, LastName, Username, Password, Email, DateOfBirth, PhoneNumber) " +
+                                   "VALUES (@ID, @FirstName, @LastName, @Username, @Password, @Email, @DateOfBirth, @PhoneNumber)";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        this.id = GenerateTeacherID();
+                        command.Parameters.AddWithValue("@ID", this.id);
+                        this.firstName = firstName;
+                        command.Parameters.AddWithValue("@FirstName", firstName);
+                        this.lastName = lastName;
+                        command.Parameters.AddWithValue("@LastName", lastName);
+                        this.username = username;
+                        command.Parameters.AddWithValue("@Username", username);
+                        this.password = password;
+                        command.Parameters.AddWithValue("@Password", password);
+                        this.email = email;
+                        command.Parameters.AddWithValue("@Email", email);
+                        this.dateOfBirth = dateOfBirth;
+                        command.Parameters.AddWithValue("@DateOfBirth", dateOfBirth);
+                        this.phoneNumber = phoneNumber;
+                        command.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+                        command.ExecuteNonQuery();
+                        result = command.ExecuteNonQuery();
+                    }
                 }
             }
+                
+            
             return result;
+        }
+
+        public bool SearchUserName(string username)
+        {
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                connection.Open();
+                string query = "SELECT Teachers WHERE Username = @Username";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Username", username);
+                    var result = command.ExecuteScalar();
+                    if (result != null)
+                    {
+                        Console.WriteLine("username already exist, choose another one");
+                        return false;
+
+                    }
+
+
+
+                }
+                return true;
+
+            }
+
+            return false;
+
         }
 
         public void Update(string id, string firstName, string lastName, string username, string password, string email, DateTime dateOfBirth, string phoneNumber)
